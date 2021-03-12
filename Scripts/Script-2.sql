e28c99eea4e074afcf202995b6c813aa3e0f7ca5
CEINMARK <CEINMARK@DESKTOP-QOO9QN8>
2021-03-12 13:17:23 +0100

    12/03

---
+++ Scripts/Script-2.sql
@@ -0,0 +1,18 @@
+select truncate(3.9286,0)
+
+select truncate(3568,-2)
+
+select ascii('Ayer') 
+
+select bin(37)
+
+select bit_length('Hola')
+
+select char(77,121,83,81,76)
+
+select char_length('hola')
+
+select concat('hola',' mundo') 
+
+select concat_ws('_','Hola','Mundo')
+
